087a12bb3c2d3203b42fce81907a94b014adb075
Michael Solovyov <m@obble.ca>
2019-10-29 08:08:48 -0700

    update local dir for travis

--- survey/app/utils/geoHelper.js
+++ survey/app/utils/geoHelper.js
@@ -17,16 +17,16 @@ const geoHelper = {
       let msg = null
       switch (error.code) {
         case error.PERMISSION_DENIED:
-          msg = 'User denied the request for Geolocation.';
+          msg = 'User denied the request for Geolocation.'
           break
         case error.POSITION_UNAVAILABLE:
-          msg = 'Location information is unavailable.';
+          msg = 'Location information is unavailable.'
           break
         case error.TIMEOUT:
-          msg = 'The request to get user location timed out.';
+          msg = 'The request to get user location timed out.'
           break
         case error.UNKNOWN_ERROR:
-          msg = 'An unknown error occurred.';
+          msg = 'An unknown error occurred.'
           break
       }
       alert(msg)
