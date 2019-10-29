6fe53793383c6423c5822b38877ca0691e1359c4
Michael Solovyov <m@obble.ca>
2019-10-29 08:45:14 -0700

    cleanup old comments

--- reporting/src/api.js
+++ reporting/src/api.js
@@ -28,22 +28,7 @@ api.post(
       `INSERT INTO duckreports (feedtime,food, amount, latitude,longitude, timezone) VALUES(:feedtime,:food, :amount,:latitude, :longitude, :timezone)`,
       req.data.payload
     )
-    /*** Assuming we're in an async function ***/
 
-    // INSERT with named parameters
-//     let insert = await db.executeStatement({
-//       secretArn: 'arn:aws:secretsmanager:us-west-2:817309612200:secret:rds-db-credentials/cluster-ONEHAAJ7P3XJLOBSF3YH3VV4ME/admin-65h5dV',
-//       resourceArn: 'arn:aws:rds:us-west-2:817309612200:cluster:duckfeedrdb',
-//       database: 'duckfeeding',
-//       sql: 'INSERT INTO muckreports (feedtime,food,latitude,longitude, timezone) VALUES(:feedtime,:food,:latitude, :longitude, :timezone)',
-//       parameters: [
-//         { name: 'feedtime', value: { stringValue: 'Cousin Oliver' } },
-//         { name: 'food', value: { longValue: 10 } },
-//         { name: 'latitude', value: { booleanValue: false } },
-//         { name: 'longitude', value: { booleanValue: false } },
-//         { name: 'timezone', value: { booleanValue: false } }
-//       ]
-// ).promise()
       return insert
   },
   { success: 201 }
