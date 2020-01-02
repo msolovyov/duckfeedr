ffa0a25f240de240ab61507de331040a2612bbf3
Michael Solovyov <m@obble.ca>
2020-01-02 10:44:25 -0800

    add wait spinner for submit along with reset of form

--- survey/app/components/feedForm.vue
+++ survey/app/components/feedForm.vue
@@ -1,5 +1,15 @@
 <template>
   <v-ons-page>
+    <v-ons-modal
+      :visible="submitting"
+    >
+      <p style="text-align: center">
+        Submitting <v-ons-icon
+          icon="fa-spinner"
+          spin
+        />
+      </p>
+    </v-ons-modal>
     <v-ons-toolbar>
       <div class="center">
         duckFeedr
@@ -120,7 +130,8 @@ export default {
 
       ],
       // state
-      submitted: false
+      submitting: false,
+      presented: false
     }
   },
   computed: {
@@ -132,6 +143,26 @@ export default {
       set (val) {
         this.feedTime = val
       }
+    },
+    // state
+    formFilled () {
+      if (this.feedTime &&
+      this.formFoodType &&
+      this.formFoodAmount &&
+      this.latitude &&
+      this.longitude &&
+      this.formDuckAmount) {
+        return true
+      } else {
+        return false
+      }
+    },
+    submitted () {
+      if (this.submitting && this.presented && this.formFilled) {
+        return true
+      } else {
+        return false
+      }
     }
 
   },
@@ -139,6 +170,12 @@ export default {
     this.nextAction()
   },
   methods: {
+    showModal () {
+      this.modalVisible = true
+    },
+    closeModal () {
+      this.modalVisible = false
+    },
     async refreshLocation () {
       const coords = await geoHelper.getCoordinates()
       this.latitude = coords.latitude
@@ -158,23 +195,25 @@ export default {
       }
       console.log(data)
       if (data.feedtime && data.food && data.amount && data.timezone && data.duckAmount) {
+        this.submitting = true
         await this.modelPresent(data)
         this.$ons.notification.alert('Your duck feeding report has been submitted')
-        this.submitted = true
       }
     },
-    resetForm () {
+    reset () {
       this.feedTime = ''
       this.formFoodType = ''
       this.formFoodAmount = ''
       this.latitude = ''
       this.longitude = ''
       this.formDuckAmount = ''
-      this.submitted = false
+      this.submitting = false
+      this.presented = false
       this.modelPresent()
     },
-    modelPresent (data) {
-      store.present(data)
+    async modelPresent (data) {
+      await store.present(data)
+      this.presented = true
       this.nextAction()
     },
     async nextAction () {
@@ -185,7 +224,7 @@ export default {
         await this.refreshLocation()
       }
       if (this.submitted) {
-        this.resetForm()
+        this.reset()
       }
     }
   }
